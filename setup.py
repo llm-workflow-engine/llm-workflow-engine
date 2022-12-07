e9ee5d972e6639d368067df42438950448cca889
Mahmoud Mabrouk <resiros@gmail.com>
2022-12-07 14:19:32 +0100

    Update setup.py
    small fix

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-
+    
 setup(
     name="chatGPT",
     version="0.1.0",
