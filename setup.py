67919fabed34aada9c7ff830833b1a0e59740ba7
Jonghyeon yeo <hybridkernal@gmail.com>
2023-02-06 10:51:58 +0900

    Update setup url

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description="A simple Python class for interacting with OpenAI's chatGPT using Playwright",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/openai/playwright-chatbot",
+    url="https://github.com/mmabrouk/chatgpt-wrapper",
     packages=find_packages(),
     install_requires=[
         "playwright",
