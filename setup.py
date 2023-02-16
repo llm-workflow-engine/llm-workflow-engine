1c2eb020cc74184a15f20508ab73dcec07bc5d5b
Mahmoud Mabrouk <resiros@gmail.com>
2023-02-16 12:11:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="chatGPT",
-    version="0.3.14",
+    version="0.3.15",
     author="Mahmoud Mabrouk",
     author_email="mahmoudmabrouk.mail@gmail.com",
     description="A simple Python class for interacting with OpenAI's chatGPT using Playwright",
