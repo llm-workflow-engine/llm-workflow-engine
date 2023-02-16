96cc2cc26c161bd6b933a9ce56913e9703e3a82c
Mahmoud Mabrouk <resiros@gmail.com>
2023-02-16 12:11:12 +0100

    changed the model to text-davinci-002-render-sha

--- chatgpt_wrapper/chatgpt.py
+++ chatgpt_wrapper/chatgpt.py
@@ -110,7 +110,7 @@ class ChatGPT:
                     "content": {"content_type": "text", "parts": [prompt]},
                 }
             ],
-            "model": "text-davinci-002-render",
+            "model": "text-davinci-002-render-sha",
             "conversation_id": self.conversation_id,
             "parent_message_id": self.parent_message_id,
             "action": "next",
