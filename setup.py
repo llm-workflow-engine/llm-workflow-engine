327bbee165b9b291d5455765ed837e4d7d0718a1
Chad Phillips <chad@apartmentlines.com>
2023-03-17 14:35:53 -0400

    find version in version.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import find_packages, setup
+import re
+from os import path
+
+FILE_DIR = path.dirname(path.abspath(path.realpath(__file__)))
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -6,9 +10,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
 with open('requirements.txt') as f:
     install_requirement = f.readlines()
 
+with open(path.join(FILE_DIR, 'chatgpt_wrapper', 'version.py')) as f:
+    version = re.match(r'^__version__ = "([\w\.]+)"$', f.read().strip())[1]
+
 setup(
     name="chatGPT",
-    version="0.6.0",
+    version=version,
     author="Mahmoud Mabrouk",
     author_email="mahmoudmabrouk.mail@gmail.com",
     description="A simple Python class for interacting with OpenAI's chatGPT using Playwright",
