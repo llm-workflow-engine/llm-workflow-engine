293b39ec57262a0804706fd5482a27a8a6e3fcc7
aryan-0102 <89297308+aryan-0102@users.noreply.github.com>
2023-03-17 13:12:25 +0530

    Update setup.py
    basic documentation, spacing, and indentation

--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@ from setuptools import find_packages, setup
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
+with open('requirements.txt') as f:                                          #installing requirements
     install_requirement = f.readlines()
 
 setup(
-    name="chatGPT",
-    version="0.6.0",
-    author="Mahmoud Mabrouk",
-    author_email="mahmoudmabrouk.mail@gmail.com",
-    description="A simple Python class for interacting with OpenAI's chatGPT using Playwright",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/mmabrouk/chatgpt-wrapper",
-    packages=find_packages(),
+    name = "chatGPT",
+    version = "0.6.0",
+    author = "Mahmoud Mabrouk",
+    author_email = "mahmoudmabrouk.mail@gmail.com",
+    description = "A simple Python class for interacting with OpenAI's chatGPT using Playwright",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/mmabrouk/chatgpt-wrapper",
+    packages = find_packages(),
     install_requires=install_requirement,
-    classifiers=[
+    classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
