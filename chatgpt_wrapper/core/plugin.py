ff555ac688833216bae4546413832a79349a2897
Chad Phillips <chad@apartmentlines.com>
2023-03-28 18:32:42 -0400

    add LLM base methods for plugins to leverage

--- chatgpt_wrapper/core/plugin.py
+++ chatgpt_wrapper/core/plugin.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from langchain.chat_models import ChatOpenAI
+
 from chatgpt_wrapper.core.config import Config
 from chatgpt_wrapper.core.logger import Logger
 
@@ -27,6 +29,34 @@ class PluginBase(ABC):
 
     def get_shell_completions(self, _base_shell_completions):
         pass
+
+    def get_default_llm_args(self):
+        return {
+            'temperature': 0,
+            'model_name': self.backend.model,
+            # TODO: This used to work on the deprecated OpenAIChat class, but now no longer works.
+            # 'prefix_messages': [
+            #     {
+            #         'role': 'system',
+            #         'content': 'You are a helpful assistant that is very good at problem solving who thinks step by step.',
+            #     },
+            # ]
+        }
+
+    def make_llm(self, args={}):
+        final_args = self.get_default_llm_args()
+        final_args.update(args)
+        llm = ChatOpenAI(**final_args)
+        return llm
+
+    def query_llm(self, messages):
+        llm = self.make_llm()
+        try:
+            result = llm(messages)
+            result_string = result.content
+        except ValueError as e:
+            return False, None, e
+        return True, result, result_string
 
 class Plugin(PluginBase):
 
