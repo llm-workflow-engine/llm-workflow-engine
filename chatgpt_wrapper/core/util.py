b74ba0fd3781ffdcb38b42e0c80a9bb7862d1a09
Chad Phillips <chad@apartmentlines.com>
2023-03-27 21:57:17 -0400

    snake_to_class() util function

--- chatgpt_wrapper/core/util.py
+++ chatgpt_wrapper/core/util.py
@@ -181,3 +181,7 @@ def get_package_root(obj):
     package_name = obj.__class__.__module__.split('.')[0]
     package_root = os.path.dirname(os.path.abspath(sys.modules[package_name].__file__))
     return package_root
+
+def snake_to_class(string):
+    parts = string.split('_')
+    return ''.join(word.title() for word in parts)
