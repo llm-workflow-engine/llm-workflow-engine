b794c56b18bf3467617c2f82a9b60c1c4932a808
Chad Phillips <chad@apartmentlines.com>
2023-03-29 11:50:12 -0400

    fix broken template tests

--- tests/unit/test_template.py
+++ tests/unit/test_template.py
@@ -1,30 +1,46 @@
 import os
+import tempfile
 import pytest
-from unittest.mock import MagicMock
+import pyperclip
+
+from jinja2 import Environment, Template
 
 from chatgpt_wrapper.core.config import Config
 from chatgpt_wrapper.core.template import TemplateManager
 import chatgpt_wrapper.core.util as util
 
-TEST_TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'test_templates')
+TEST_DIR = os.path.join(tempfile.gettempdir(), 'chatgpt_wrapper_test')
+TEST_CONFIG_DIR = os.path.join(TEST_DIR, 'config')
+TEST_DATA_DIR = os.path.join(TEST_DIR, 'data')
+TEST_PROFILE = 'test'
 
 
 @pytest.fixture
 def test_config():
-    config = Config()
-    config.config_dir = TEST_TEMPLATE_DIR
-    config.config_profile_dir = TEST_TEMPLATE_DIR
+    util.remove_and_create_dir(TEST_CONFIG_DIR)
+    util.remove_and_create_dir(TEST_DATA_DIR)
+    config = Config(TEST_CONFIG_DIR, TEST_DATA_DIR, profile=TEST_PROFILE)
     return config
 
 
 @pytest.fixture
 def template_manager(test_config):
-    return TemplateManager(config=test_config)
+    template_manager = TemplateManager(config=test_config)
+    template_manager.load_templates()
+    return template_manager
 
+def make_template_file(template_manager, template_name, content=None):
+    template_dir = template_manager.template_dirs[0]
+    util.create_file(template_dir, template_name, content)
+    template_manager.load_templates()
+
+def remove_template_file(template_manager, template_name):
+    template_dir = template_manager.template_dirs[0]
+    os.remove(os.path.join(template_dir, template_name))
 
 def test_init(template_manager):
     assert isinstance(template_manager, TemplateManager)
-    assert len(template_manager.template_dirs) == 2
+    assert len(template_manager.template_dirs) > 0
     assert template_manager.templates == []
 
 
@@ -36,16 +52,18 @@ def test_template_builtin_variables(template_manager):
 
 def test_ensure_template(template_manager):
     template_name = "existent_template.md"
-    result = template_manager.ensure_template(template_name)
-    assert result[0] is True
-    assert result[1] == template_name
-    assert "exists" in result[2]
+    make_template_file(template_manager, template_name)
+    success, name, user_message = template_manager.ensure_template(template_name)
+    remove_template_file(template_manager, template_name)
+    assert success is True
+    assert name == template_name
+    assert "exists" in user_message
 
     template_name = "nonexistent_template.md"
-    result = template_manager.ensure_template(template_name)
-    assert result[0] is False
-    assert result[1] == template_name
-    assert "not found" in result[2]
+    success, name, user_message = template_manager.ensure_template(template_name)
+    assert success is False
+    assert name == template_name
+    assert "not found" in user_message
 
 
 def test_extract_metadata_keys(template_manager):
@@ -55,9 +73,9 @@ def test_extract_metadata_keys(template_manager):
         'custom': 'Custom Value'
     }
     keys = ['title', 'custom']
-    result = template_manager.extract_metadata_keys(keys, metadata)
-    assert result[0] == {'description': 'Test Description'}
-    assert result[1] == {'title': 'Test Title', 'custom': 'Custom Value'}
+    metadata, extracted_keys = template_manager.extract_metadata_keys(keys, metadata)
+    assert metadata == {'description': 'Test Description'}
+    assert extracted_keys == {'title': 'Test Title', 'custom': 'Custom Value'}
 
 
 def test_extract_template_run_overrides(template_manager):
@@ -67,25 +85,31 @@ def test_extract_template_run_overrides(template_manager):
         'model_customizations': {'option': 'value'},
         'custom': 'Custom Value'
     }
-    result = template_manager.extract_template_run_overrides(metadata)
-    assert result[0] == {'custom': 'Custom Value'}
-    assert result[1] == {'title': 'Test Title', 'model_customizations': {'option': 'value'}}
+    metadata, overrides = template_manager.extract_template_run_overrides(metadata)
+    assert metadata == {'custom': 'Custom Value'}
+    assert overrides == {'title': 'Test Title', 'model_customizations': {'option': 'value'}}
 
 
 def test_build_message_from_template(template_manager):
-    template_name = "existent_template.md"
-    result = template_manager.build_message_from_template(template_name, {'name': 'John Doe'})
-    assert 'Hello, John Doe' in result[0]
-    assert result[1] == {'title': 'Existent Template'}
+    template_name = "hello.md"
+    template_content = """
+---
+title: Existent Template
+---
+Hello, {{ name }}
+"""
+    make_template_file(template_manager, template_name, template_content)
+    message, overrides = template_manager.build_message_from_template(template_name, {'name': 'John Doe'})
+    remove_template_file(template_manager, template_name)
+    assert 'Hello, John Doe' in message
+    assert overrides == {'title': 'Existent Template'}
 
 
 def test_process_template_builtin_variables(template_manager, monkeypatch):
-    util.paste_from_clipboard = MagicMock(return_value="clipboard_text")
-    monkeypatch.setattr(template_manager, 'template_builtin_variables', util.paste_from_clipboard)
-
+    pyperclip.copy("clipboard_text")
     variables = ['clipboard']
-    result = template_manager.process_template_builtin_variables("existent_template.md", variables)
-    assert result == {'clipboard': 'clipboard_text'}
+    substitutions = template_manager.process_template_builtin_variables("existent_template.md", variables)
+    assert substitutions == {'clipboard': 'clipboard_text'}
 
 
 def test_make_template_dirs(template_manager, tmpdir):
@@ -103,31 +127,16 @@ def test_load_templates(template_manager):
     assert isinstance(template_manager.templates, list)
 
 def test_get_template_and_variables_found(template_manager):
-    template_name = "test_template.jinja"
-    template_content = "{{ some_variable }}"
-
-    # Mock template environment
-    mock_env = MagicMock()
-    mock_env.get_template.return_value = Template(template_content)
-    mock_env.loader.get_source.return_value = template_content
-    mock_env.parse.return_value = meta.parse(template_content)
-
-    template_manager.templates_env = mock_env
-
+    template_name = "test.md"
+    template_content = "Test {{ some_variable }}"
+    make_template_file(template_manager, template_name, template_content)
     template, variables = template_manager.get_template_and_variables(template_name)
-
+    remove_template_file(template_manager, template_name)
     assert isinstance(template, Template)
     assert "some_variable" in variables
 
 def test_get_template_and_variables_not_found(template_manager):
-    template_name = "non_existent_template.jinja"
-
-    # Mock template environment
-    mock_env = MagicMock()
-    mock_env.get_template.side_effect = TemplateNotFound(template_name)
-    template_manager.templates_env = mock_env
-
+    template_name = "non_existent_template.md"
     template, variables = template_manager.get_template_and_variables(template_name)
-
     assert template is None
     assert variables is None
