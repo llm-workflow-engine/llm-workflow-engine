4e19ba3978d66a0428b9373f88b6c3213dc0da4f
Chad Phillips <chad@apartmentlines.com>
2023-04-01 14:00:44 -0400

    update docker entrypoint instructions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "----- Please follow these steps -----"
-echo '1) Run `chatgpt`, follow the steps to create the first user'
-echo '2) Have a nice chat'
+echo '1) Run command `docker exec -it chatgpt-wrapper-container /bin/bash -c \"chatgpt\"`'
+echo '2) Follow the instructions to create the first user'
+echo '3) Have a nice chat'
 
 # python # just holding
 python chatgpt_wrapper/gpt_api.py
