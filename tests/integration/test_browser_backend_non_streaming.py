4686cd0655d3e590d128538513047388109c9d7a
Chad Phillips <chad@apartmentlines.com>
2023-04-11 10:11:58 -0700

    clean up browser integration test

--- tests/integration/test_browser_backend_non_streaming.py
+++ tests/integration/test_browser_backend_non_streaming.py
@@ -5,14 +5,13 @@ from chatgpt_wrapper.core.config import Config
 
 def test_browser_backend_non_streaming():
     config = Config(profile='test')
-    config.set('backend', 'browser')
     if __name__ == '__main__':
         config.set('browser.debug', True)
     config.set('debug.log.enabled', True)
     gpt = ChatGPT(config)
     gpt.launch_browser()
-    response1 = gpt.ask("Say, bot1!")
-    print(response1)
+    success, response1, user_message = gpt.ask("Say, bot1!")
+    assert success
     assert response1
 
 if __name__ == '__main__':
