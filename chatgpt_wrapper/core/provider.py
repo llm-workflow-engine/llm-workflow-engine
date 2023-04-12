0809912a891005db37aec13a4143fe705376c429
Chad Phillips <chad@apartmentlines.com>
2023-04-11 22:20:29 -0700

    PresetValue class, functionality to set model customizations

--- chatgpt_wrapper/core/provider.py
+++ chatgpt_wrapper/core/provider.py
@@ -1,8 +1,70 @@
 from abc import abstractmethod
 
 from chatgpt_wrapper.core.plugin import Plugin
+from chatgpt_wrapper.core import util
+
+class PresetValue:
+    def __init__(self, value_type, min_value=None, max_value=None, options={}, include_none=False, private=False):
+        self.value_type = value_type
+        self.min_value = min_value
+        self.max_value = max_value
+        self.options = options
+        self.include_none = include_none
+        self.private = private
+        self.completions = {}
+        self.build_completions()
+
+    def build_completions(self):
+        if self.value_type == bool:
+            self.completions = {
+                'true': None,
+                'false': None,
+            }
+        elif self.value_type == int:
+            if self.min_value is not None and self.max_value is not None:
+                self.completions = util.list_to_completion_hash(range(self.min_value, self.max_value + 1))
+        elif self.value_type == float:
+            if self.min_value is not None and self.max_value is not None:
+                self.completions = util.float_range_to_completions(self.min_value, self.max_value)
+        elif self.value_type == str:
+            self.completions = util.list_to_completion_hash(self.options)
+        elif self.value_type == dict:
+            pass
+        else:
+            raise ValueError("Invalid value type provided. Must be one of the following: str, int, float, bool")
+        if self.value_type != dict and self.include_none:
+            self.completions['None'] = None
+
+    def cast(self, value):
+        if value == 'None':
+            return True, None, None
+        if self.value_type == bool:
+            if value.lower() in ['true', 't', '1']:
+                return True, True, None
+            elif value.lower() in ['false', 'f', '0']:
+                return True, False, None
+            else:
+                return False, None, "Invalid value provided. Must be one of the following: true, false"
+        elif self.value_type == int:
+            try:
+                return True, int(value), None
+            except ValueError:
+                return False, None, "Invalid value provided. Must be an integer"
+        elif self.value_type == float:
+            try:
+                return True, float(value), None
+            except ValueError:
+                return False, None, "Invalid value provided. Must be a float"
+        elif self.value_type == str:
+            return True, str(value), None
+        else:
+            return False, None, "Invalid value type provided. Must be one of the following: str, int, float, bool"
 
 class ProviderBase(Plugin):
+
+    def __init__(self, config=None, customizations=None):
+        super().__init__(config)
+        self.set_customizations(self.default_customizations())
 
     @property
     def plugin_type(self):
@@ -19,8 +81,75 @@ class ProviderBase(Plugin):
     def default_config(self):
         return {}
 
+    def default_customizations(self):
+        llm_class = self.llm_factory()
+        llm = llm_class()
+        llm_defaults = llm.dict()
+        custom_config = self.customization_config()
+        defaults = {k: v for k, v in llm_defaults.items() if k in custom_config}
+        return defaults
+
+    def calculate_customization_value(self, orig_keys, new_value):
+        if isinstance(orig_keys, str):
+            orig_keys = orig_keys.split('.')
+        keys = orig_keys.copy()
+        config = self.customization_config()
+        while keys:
+            key = keys.pop(0)
+            if key in config:
+                config = config[key]
+                if config == dict:
+                    return True, new_value, "Found dict key."
+                elif isinstance(config, PresetValue):
+                    success, new_value, user_message = config.cast(new_value)
+                    if success:
+                        return True, new_value, "Found preset value."
+                    return False, None, user_message
+            else:
+                break
+        return False, None, f"Invalid key {key}."
+
+    def set_customization_value(self, keys, new_value):
+        if isinstance(keys, str):
+            keys = keys.split('.')
+        success, new_value, user_message = self.calculate_customization_value(keys, new_value)
+        if success:
+            self.set_value(keys, new_value)
+            return success, self.customizations, f"Set {'.'.join(keys)} to {new_value}"
+        else:
+            return success, new_value, user_message
+
+    def set_value(self, keys, value):
+        customizations = self.customizations
+        for key in keys[:-1]:
+            customizations = customizations.setdefault(key, {})
+        customizations[keys[-1]] = value
+
+    def set_customizations(self, customizations):
+        self.customizations = customizations
+
+    def customizations_to_completions(self):
+        completions = {}
+        for key, value in self.customization_config().items():
+            if value == dict:
+                continue
+            elif isinstance(value, dict):
+                completions[key] = self.customizations_to_completions(value)
+            elif isinstance(value, object):
+                completions[key] = value.completions
+            else:
+                completions[key] = value
+        return completions
+
+    def get_shell_completions(self, base_shell_completions):
+        base_shell_completions[util.command_with_leader('model')] = self.customizations_to_completions()
+
 class Provider(ProviderBase):
 
     @abstractmethod
     def llm_factory(self):
+        pass
+
+    @abstractmethod
+    def customization_config(self):
         pass
--- chatgpt_wrapper/plugins/provider_chat_openai.py
+++ chatgpt_wrapper/plugins/provider_chat_openai.py
@@ -1,8 +1,24 @@
 from langchain.chat_models.openai import ChatOpenAI
 
-from chatgpt_wrapper.core.provider import Provider
+from chatgpt_wrapper.core.provider import Provider, PresetValue
+from chatgpt_wrapper.core import constants
 
 class ProviderChatOpenai(Provider):
 
     def llm_factory(self):
         return ChatOpenAI
+
+    def customization_config(self):
+        return {
+            'verbose': PresetValue(bool),
+            'model_name': PresetValue(str, options=constants.OPENAPI_CHAT_RENDER_MODELS),
+            'temperature': PresetValue(float, min_value=0.0, max_value=2.0),
+            'model_kwargs': dict,
+            'openai_api_key': PresetValue(str, include_none=True),
+            'openai_organization': PresetValue(str, include_none=True),
+            'request_timeout': PresetValue(int),
+            'max_retries': PresetValue(int, 1, 10),
+            'streaming': PresetValue(bool),
+            'n': PresetValue(int, 1, 10),
+            'max_tokens': PresetValue(int, include_none=True),
+        }
