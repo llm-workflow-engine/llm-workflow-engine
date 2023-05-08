769f9dbc160dfa52bf0201151a17038c00653724
Chad Phillips <chad@apartmentlines.com>
2023-05-07 20:09:57 -0400

    add openai provider

---
+++ chatgpt_wrapper/plugins/provider_openai.py
@@ -0,0 +1,54 @@
+from langchain.llms import OpenAI
+
+from chatgpt_wrapper.core.provider import Provider, PresetValue
+
+class ProviderOpenai(Provider):
+
+    @property
+    def capabilities(self):
+        return {
+            'streaming': True,
+            'validate_models': False,
+            'models': {
+                'text-ada-001': {
+                    'max_tokens': 2049,
+                },
+                'text-babbage-001': {
+                    'max_tokens': 2049,
+                },
+                'text-curie-001': {
+                    'max_tokens': 2049,
+                },
+                'text-davinci-001': {
+                    'max_tokens': 2049,
+                },
+                'text-davinci-002': {
+                    'max_tokens': 2049,
+                },
+                'text-davinci-003': {
+                    'max_tokens': 2049,
+                },
+            }
+        }
+
+    @property
+    def default_model(self):
+        return 'text-davinci-003'
+
+    def llm_factory(self):
+        return OpenAI
+
+    def customization_config(self):
+        return {
+            'model_name': PresetValue(str, options=self.available_models),
+            'temperature': PresetValue(float, min_value=0.0, max_value=2.0),
+            'openai_api_key': PresetValue(str, include_none=True, private=True),
+            'openai_organization': PresetValue(str, include_none=True, private=True),
+            'request_timeout': PresetValue(int),
+            'streaming': PresetValue(bool),
+            'n': PresetValue(int, 1, 10),
+            'max_tokens': PresetValue(int, include_none=True),
+            'top_p': PresetValue(float, min_value=0.0, max_value=1.0),
+            'frequency_penalty': PresetValue(float, min_value=-2.0, max_value=2.0),
+            'presence_penalty': PresetValue(float, min_value=-2.0, max_value=2.0),
+        }
