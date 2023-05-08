640dcffc98946577e2fbd6aeb05e6f2f9715ab57
Chad Phillips <chad@apartmentlines.com>
2023-05-08 14:20:00 -0400

    enhance browser backend test, add wait arg

--- tests/integration/test_browser_backend_non_streaming.py
+++ tests/integration/test_browser_backend_non_streaming.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 
+import time
+import sys
+
 from chatgpt_wrapper.core.config import Config
 from chatgpt_wrapper.backends.browser.backend import BrowserBackend
 
-def test_browser_backend_non_streaming():
+def test_browser_backend_non_streaming(wait=None):
     config = Config(profile='test')
     if __name__ == '__main__':
         config.set('browser.debug', True)
     config.set('debug.log.enabled', True)
     gpt = BrowserBackend(config)
     gpt.launch_browser()
+    if wait is not None:
+        print(f"Waiting {wait} seconds...")
+        time.sleep(wait)
     success, response1, user_message = gpt.ask("Say, bot1!")
     assert success
     assert response1
+    if success:
+        print(response1)
 
 if __name__ == '__main__':
-    test_browser_backend_non_streaming()
+    wait = int(sys.argv[1]) if len(sys.argv) > 1 else None
+    test_browser_backend_non_streaming(wait)
