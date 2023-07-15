5b294742f3ef8b487ea2b7906c4f579de8a9ac7e
Chad Phillips <chad@apartmentlines.com>
2023-07-15 16:45:55 -0400

    always append to existing debug log

--- lwe/core/logger.py
+++ lwe/core/logger.py
@@ -16,7 +16,7 @@ class Logger():
         log_console_handler.setLevel(config.get('log.console.level'))
         logger.addHandler(log_console_handler)
         if config.get('debug.log.enabled'):
-            log_file_handler = logging.FileHandler(config.get('debug.log.filepath'))
+            log_file_handler = logging.FileHandler(config.get('debug.log.filepath'), 'a')
             log_file_handler.setFormatter(logging.Formatter(config.get('debug.log.format')))
             log_file_handler.setLevel(config.get('debug.log.level'))
             logger.addHandler(log_file_handler)
