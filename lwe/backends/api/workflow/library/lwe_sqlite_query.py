39f2fa561d583e07849790cfa8facef4e5952312
Chad Phillips <chad@apartmentlines.com>
2023-07-15 14:27:58 -0400

    add lwe_sqlite_query Ansible module, example SQLite db

---
+++ lwe/backends/api/workflow/library/lwe_sqlite_query.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+
+import sqlite3
+from ansible.module_utils.basic import AnsibleModule
+
+from lwe.core.config import Config
+from lwe.core.logger import Logger
+
+config = Config()
+config.set('debug.log.enabled', True)
+log = Logger(__name__, config)
+
+DOCUMENTATION = r'''
+---
+module: lwe_sqlite_query
+short_description: Run a query against a SQLite database
+description:
+    - This module runs a query against a specified SQLite database and stores any returned data in a structured format.
+options:
+    db:
+      description:
+          - The path to the SQLite database file.
+      type: str
+      required: true
+    query:
+      description:
+          - The SQL query to execute.
+      type: str
+      required: true
+author:
+    - Chad Phillips (@thehunmonkgroup)
+'''
+
+EXAMPLES = r'''
+  - name: Run a SELECT query against a SQLite database
+    lwe_sqlite_query:
+      db: "/path/to/your/database.db"
+      query: "SELECT * FROM your_table"
+'''
+
+RETURN = r'''
+  data:
+      description: The data returned from the query.
+      type: list
+      returned: success
+  row_count:
+      description: The number of rows returned from the query.
+      type: int
+      returned: success
+'''
+
+def run_query(db, query):
+    conn = sqlite3.connect(db)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute(query)
+    data = [dict(row) for row in cursor.fetchall()]
+    row_count = len(data)
+    conn.close()
+    return data, row_count
+
+def main():
+
+    result = dict(
+        changed=False,
+        response=dict()
+    )
+
+    module = AnsibleModule(
+        argument_spec=dict(
+            db=dict(type='str', required=True),
+            query=dict(type='str', required=True),
+        ),
+        supports_check_mode=True,
+    )
+    db = module.params['db']
+    query = module.params['query']
+
+    if module.check_mode:
+        module.exit_json(**result)
+
+    try:
+        log.debug(f"Running query on database '{db}': {query}")
+        data, row_count = run_query(db, query)
+        result['changed'] = True
+        result['data'] = data
+        result['row_count'] = row_count
+        module.exit_json(**result)
+    except Exception as e:
+        result['failed'] = True
+        message = f"Failed to run query: {query}, error: {e}"
+        log.error(message)
+        module.fail_json(msg=message, **result)
+
+if __name__ == '__main__':
+    main()
