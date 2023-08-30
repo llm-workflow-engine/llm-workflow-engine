afb43bb44841ba8d30d5509d1ac92e76cd65cb5e
Chad Phillips <chad@apartmentlines.com>
2023-08-30 16:46:44 -0400

    add FakeBackend for testing

--- tests/base.py
+++ tests/base.py
@@ -4,6 +4,7 @@ import pytest
 
 from lwe.core.config import Config
 import lwe.core.util as util
+from lwe.core.backend import Backend
 
 TEST_DIR = os.path.join(tempfile.gettempdir(), 'lwe_test')
 TEST_CONFIG_DIR = os.path.join(TEST_DIR, 'config')
@@ -17,3 +18,28 @@ def test_config():
     util.remove_and_create_dir(TEST_DATA_DIR)
     config = Config(TEST_CONFIG_DIR, TEST_DATA_DIR, profile=TEST_PROFILE)
     return config
+
+
+class FakeBackend(Backend):
+    name = "api"
+
+    def conversation_data_to_messages(self, conversation_data):
+        pass
+
+    def delete_conversation(self, uuid=None):
+        pass
+
+    def set_title(self, title, conversation_id=None):
+        pass
+
+    def get_history(self, limit=20, offset=0):
+        pass
+
+    def get_conversation(self, uuid=None):
+        pass
+
+    def ask_stream(self, input: str, request_overrides: dict):
+        pass
+
+    def ask(self, input: str, request_overrides: dict):
+        pass
