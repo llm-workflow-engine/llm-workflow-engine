9ca7e189e84402326f923bdfd4f5837aafdc5155
Chad Phillips <chad@apartmentlines.com>
2023-08-31 12:11:27 -0400

    add provider_fake_llm

---
+++ lwe/plugins/provider_fake_llm.py
@@ -0,0 +1,55 @@
+from langchain.llms.fake import FakeListLLM
+
+from lwe.core.provider import Provider, PresetValue
+from lwe.core import constants
+
+DEFAULT_RESPONSES = [
+    'test response',
+]
+
+
+class CustomFakeListLLM(FakeListLLM):
+    model_name: str
+
+    def __init__(self, **kwargs):
+        if not kwargs.get('model_name'):
+            kwargs['model_name'] = constants.API_BACKEND_DEFAULT_MODEL
+        if not kwargs.get('responses'):
+            kwargs['responses'] = DEFAULT_RESPONSES
+        super().__init__(**kwargs)
+
+class ProviderFakeLlm(Provider):
+    """
+    Fake LLM provider.
+    """
+
+    @property
+    def capabilities(self):
+        return {
+            'chat': True,
+            'validate_models': False,
+            'models': {
+                'gpt-3.5-turbo': {
+                    'max_tokens': 4096,
+                },
+                'gpt-4': {
+                    'max_tokens': 8192,
+                },
+            }
+        }
+
+    @property
+    def default_model(self):
+        return constants.API_BACKEND_DEFAULT_MODEL
+
+    def prepare_messages_method(self):
+        return self.prepare_messages_for_llm_chat
+
+    def llm_factory(self):
+        return CustomFakeListLLM
+
+    def customization_config(self):
+        return {
+            'responses': None,
+            'model_name': PresetValue(str, options=self.available_models),
+        }
