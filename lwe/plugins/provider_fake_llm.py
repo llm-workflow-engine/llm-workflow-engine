af106025a9cf27dfff4b6859516feccc0922724c
Chad Phillips <chad@apartmentlines.com>
2023-09-04 19:22:56 -0400

    leverage FakeMessagesListChatModel for fake LLM provider

--- lwe/plugins/provider_fake_llm.py
+++ lwe/plugins/provider_fake_llm.py
@@ -1,22 +1,62 @@
-from langchain.chat_models.fake import FakeListChatModel
+from typing import Any, Iterator, List, Optional, Union
+
+from langchain.schema.messages import (
+    AIMessage,
+    AIMessageChunk,
+    BaseMessage,
+)
+
+from langchain.callbacks.manager import (
+    CallbackManagerForLLMRun,
+)
+from langchain.schema.output import ChatGenerationChunk
+
+from langchain.chat_models.fake import FakeMessagesListChatModel
 
 from lwe.core.provider import Provider, PresetValue
 from lwe.core import constants
 
-DEFAULT_RESPONSES = [
-    'test response',
-]
+DEFAULT_RESPONSE_MESSAGE = "test response"
 
 
-class CustomFakeListChatModel(FakeListChatModel):
+class CustomFakeMessagesListChatModel(FakeMessagesListChatModel):
     model_name: str
 
     def __init__(self, **kwargs):
         if not kwargs.get('model_name'):
             kwargs['model_name'] = constants.API_BACKEND_DEFAULT_MODEL
         if not kwargs.get('responses'):
-            kwargs['responses'] = DEFAULT_RESPONSES
+            kwargs['responses'] = []
         super().__init__(**kwargs)
+
+    def _call(
+        self,
+        messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> BaseMessage:
+        if not self.responses:
+            self.responses = [
+                AIMessage(content=DEFAULT_RESPONSE_MESSAGE),
+            ]
+        return super()._call(messages, stop, run_manager, **kwargs)
+
+    def _stream(
+        self,
+        messages: List[BaseMessage],
+        stop: Union[List[str], None] = None,
+        run_manager: Union[CallbackManagerForLLMRun, None] = None,
+        **kwargs: Any,
+    ) -> Iterator[ChatGenerationChunk]:
+        if not self.responses:
+            self.responses = [
+                [
+                    AIMessageChunk(content=DEFAULT_RESPONSE_MESSAGE),
+                ],
+            ]
+        return super()._stream(messages, stop, run_manager, **kwargs)
+
 
 class ProviderFakeLlm(Provider):
     """
@@ -46,7 +86,7 @@ class ProviderFakeLlm(Provider):
         return self.prepare_messages_for_llm_chat
 
     def llm_factory(self):
-        return CustomFakeListChatModel
+        return CustomFakeMessagesListChatModel
 
     def customization_config(self):
         return {
