6ee5b7fcf79bebd6de1489e93de71bc29c582c67
Chad Phillips <chad@apartmentlines.com>
2023-09-05 18:04:57 -0400

    update function cache tests for recent changes

--- tests/unit/test_function_cache.py
+++ tests/unit/test_function_cache.py
@@ -1,3 +1,5 @@
+import pytest
+
 from lwe.core.function_cache import FunctionCache
 
 
@@ -26,11 +28,16 @@ def test_function_cache_init_with_customizations(test_config, function_manager):
     assert 'test_function' in function_cache.functions
 
 
-def test_function_cache_add(test_config, function_manager):
+def test_function_cache_add_valid(test_config, function_manager):
     function_cache = make_function_cache(test_config, function_manager)
-    function_manager.functions = {}
-    assert function_cache.add('test_function') is False
-    assert len(function_cache.functions) == 0
+    function_manager.functions = {'test_function': 'test_function_path'}
+    assert function_cache.add('test_function') is True
+    assert 'test_function' in function_cache.functions
+    assert len(function_cache.functions) == 1
+
+
+def test_function_cache_add_valid_only_adds_function_once(test_config, function_manager):
+    function_cache = make_function_cache(test_config, function_manager)
     function_manager.functions = {'test_function': 'test_function_path'}
     assert function_cache.add('test_function') is True
     assert 'test_function' in function_cache.functions
@@ -39,15 +46,42 @@ def test_function_cache_add(test_config, function_manager):
     assert len(function_cache.functions) == 1
 
 
-def test_function_cache_add_langchain_tool(test_config, function_manager):
-    function_cache = make_function_cache(test_config, function_manager)
-    function_manager.functions = {'Langchain-BadTool': 'test_function_path'}
-    assert function_cache.add('Langchain-BadTool') is False
+def test_function_cache_add_valid_from_customizations(test_config, function_manager):
+    customizations = {'model_kwargs': {'functions': ['test_function']}}
+    function_manager.functions = {'test_function': 'test_function_path'}
+    function_cache = make_function_cache(test_config, function_manager, customizations)
+    assert 'test_function' in function_cache.functions
+    assert len(function_cache.functions) == 1
+
+
+def test_function_cache_add_skip_non_string_function_definition_from_customizations(test_config, function_manager):
+    customizations = {'model_kwargs': {'functions': dict()}}
+    function_cache = make_function_cache(test_config, function_manager, customizations)
     assert len(function_cache.functions) == 0
+
+
+def test_function_cache_add_bad_function(test_config, function_manager):
+    function_cache = make_function_cache(test_config, function_manager)
+    function_manager.functions = {}
+    with pytest.raises(ValueError) as excinfo:
+        function_cache.add('test_function')
+    assert "test_function not found" in str(excinfo.value)
+
+
+def test_function_cache_add_valid_langchain_tool(test_config, function_manager):
+    function_cache = make_function_cache(test_config, function_manager)
     function_manager.functions = {'Langchain-ShellTool': 'test_function_path'}
     assert function_cache.add('Langchain-ShellTool') is True
     assert 'Langchain-ShellTool' in function_cache.functions
     assert len(function_cache.functions) == 1
+
+
+def test_function_cache_add_bad_langchain_tool(test_config, function_manager):
+    function_cache = make_function_cache(test_config, function_manager)
+    function_manager.functions = {'Langchain-BadTool': 'test_function_path'}
+    with pytest.raises(ValueError) as excinfo:
+        function_cache.add('Langchain-BadTool')
+    assert "Langchain-BadTool not found" in str(excinfo.value)
 
 
 def test_function_cache_add_message_functions(test_config, function_manager):
